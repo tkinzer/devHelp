8c85cbbb7fedfea893d4e78c4b793c0828417871
Tim Kinzer <tkinzer@users.noreply.github.com>
2015-01-01 18:55:52 -0800

    Adding simple php

---
+++ simple-php.php
@@ -0,0 +1,35 @@
+<!DOCTYPE>
+<?php
+//SESSION CODE
+  //CHECK CREDENTIALS
+  
+  //LOAD NECESSARY BACKEND DATA
+?>
+<html>
+<head>
+<!-- ADD META -->
+
+<!-- ADD DESC AND TITLE -->
+<title></title>
+
+<!-- CSS INCLUDES -->
+
+<!-- STYLING -->
+<style>
+
+</style>
+</head>
+<body>
+<!-- JS HEADER *** Not Recommended -->
+
+
+<!-- DOM ELEMENTS -->
+
+
+<!-- JS INCLUDES -->
+
+
+<!-- JS -->
+
+</body>
+</html>
